8b181b8704d9cb2c8eb5d45e87c172b68a94c06f
lilong <lilong1919119@foxmail.com>
2017-09-19 15:56:43 +0800

    添加了修改密码模块，编写模板自动跳转

--- users/views.py
+++ users/views.py
@@ -5,13 +5,14 @@ from .forms import RegisterForm
 
 def register(request):
     redirect_to=request.POST.get('next',request.GET.get('next',''))
+    #从post或者get请求中获取next的值
     if request.method=='POST':  #如果是post请求
         form=RegisterForm(request.POST)   #将post数据实例化为一个表单
 
         if form.is_valid():
             form.save()
             if redirect_to:
-                return redirect(redirect_to)
+                return redirect(redirect_to)  #next值存在，则调回原页面
             else:
                 return redirect('/')
 
@@ -22,4 +23,4 @@ def register(request):
     return render(request,'users/register.html',context={'form':form,'next':redirect_to})
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html')  #首页
